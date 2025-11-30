9ac8f3357c72ea14cd2a159d15de5d26a1b661f7
CG-Dillon <cgdillon69@gmail.com>
2025-11-30 04:18:51 -0500

    Fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,13 @@ import vueI18n from '@intlify/vite-plugin-vue-i18n'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const baseUrl = mode === 'production' ? env.BASE_URL : '/'
 
   return {
-         base: './'
+    // Ensures images and assets load correctly in production on DigitalOcean
+    base: './',
+
     build: {
-      chunkSizeWarningLimit: 2600,
+      chunkSizeWarningLimit: 2500,
       rollupOptions: {
         output: {
           manualChunks(id) {
